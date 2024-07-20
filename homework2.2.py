3a8edd71043e1175f3754055a1a8a0c311b2081f
Anastasiya <tiger.ks@mail.ru>
2024-07-20 13:16:11 +0300

    Add files via upload

---
+++ homework2.2.py
@@ -0,0 +1,12 @@
+first = 33
+second = 72
+third = 33
+print(first)
+print(second)
+print(third)
+if 72 == 33 and 33 == 72 or 33 == 33:
+    print(0)
+if 33 == 72 == 33:
+    print(3)
+elif 72 == 33 or 33 == 72 or 33 == 33:
+    print(2)
